a5ca67ac3b2ddca09fc8dbbf7cf5414f1bde9898
Muhammad Abriq <mrabriq756@gmail.com>
2025-11-21 15:04:54 +0500

    database.py

--- app/db/database.py
+++ app/db/database.py
@@ -8,8 +8,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # ---------------------------
 # DATABASE URL
 # ---------------------------
-DATABASE_URL = 'postgresql://neondb_owner:npg_wrNv5XO2qpZK@ep-little-heart-a1v7rmta-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-# ENGINE & SESSION
+DATABASE_URL = 'put your postgre Sql database url' 
+#session engine
 # ---------------------------
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
